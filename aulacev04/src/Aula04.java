ab72fcb094c568dbbb0133a97c6578c3e2975eef
Guilherme Silva <sgsouza.173@gmail.com>
2024-06-21 15:29:10 -0300

    Atualizando

---
+++ aulacev04/src/Aula04.java
@@ -0,0 +1,8 @@
+public class Aula04 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("BIC", 0.4f, "Amarela");
+        c1.status();
+        Caneta c2 = new Caneta("Faber-Castel", 1.5f, "Verde");
+        c2.status();
+    }
+}
